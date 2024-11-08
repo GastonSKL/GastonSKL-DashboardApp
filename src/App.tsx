f90c0a954f6c00969226169212d9ec8acad48338
GastonSKL <gastonlskamlec@gmail.com>
2024-11-08 13:53:14 -0300

    [add][08/11/2024][dashboardapp] Add edit functionality to compans as well as contacts table

--- src/App.tsx
+++ src/App.tsx
@@ -23,6 +23,7 @@ import { Home, ForgotPassword, Login, Register, CompanyList } from "./pages";
 import {Layout} from './components/layout/index'
 import { resources } from "./config/resources";
 import Create from "./pages/companies/create";
+import EditPage from "./pages/companies/edit";
 
 function App() {
   return (
@@ -62,6 +63,7 @@ function App() {
                   <Route path="/companies" >
                     <Route index element={<CompanyList />}/>
                     <Route path="new" element={<Create />}/>
+                    <Route path="edit/:id" element={<EditPage />}/>
                   </Route>
                 </Route>
                 <Route path="/register" element={<Register />} />
